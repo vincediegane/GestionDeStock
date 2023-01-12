cea1e8114c3098dbb81d6d07fb7ef3732f442779
vincent diegane faye <vincediegane@gmail.com>
2023-01-12 16:47:45 +0100

    adding .gitignore file

---
+++ src/test/java/com/vincent/stock/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
